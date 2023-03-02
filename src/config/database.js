d9b32855518d8ba4534fb6364752b71c159eabb1
gsnorberto <sn_gabriel@outlook.com>
2023-03-02 14:39:33 -0300

    refact: muda as configurações do database

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,11 @@ const { Pool } = pg
 
 const connection = {
     connectionString: process.env.DATABASE_URL,
+    ...(process.env.NODE_ENV === "production" && {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    }),
 };
 
 export const db = new Pool(connection)
